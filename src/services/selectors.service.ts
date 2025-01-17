a79ca61db0b4313408bd01059af700ea9f4e0ffb
diego <diegoolmedog96@gmail.com>
2025-01-16 23:02:46 -0300

    feat: added selector service

---
+++ src/services/selectors.service.ts
@@ -0,0 +1,26 @@
+import { GenericRequestParams } from '@/interface/request-api-params';
+import { IResponseApiListViewGet } from '@/interface/response-api-get';
+import { api } from '@/lib/api';
+
+export type SelectOption = {
+  value: number;
+  label: string;
+};
+
+export interface ApiGenericSelectorsRequestParams extends GenericRequestParams {
+  endpoint: string;
+}
+
+export type ApiGenericSelectorResponse = IResponseApiListViewGet<SelectOption>;
+
+export const selectorsService = {
+  async listView(
+    params: ApiGenericSelectorsRequestParams
+  ): Promise<ApiGenericSelectorResponse> {
+    const { endpoint, ...rest } = params;
+    const response = await api.get<ApiGenericSelectorResponse>(`${endpoint}/`, {
+      params: rest,
+    });
+    return response.data;
+  },
+};
