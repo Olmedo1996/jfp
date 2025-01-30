88244f723fbfbe6aeb4cff00dd01b740cde8030e
diego <diegoolmedog96@gmail.com>
2025-01-29 22:13:56 -0300

    style: change input error focus style

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,24 +4,24 @@ import { cn } from '@/lib/utils';
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
-  error?: string;
+  error?: string | boolean;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, error, ...props }, ref) => {
     return (
-      <div className="flex flex-col gap-1">
-        <input
-          type={type}
-          className={cn(
-            'border-input bg-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
-            className,
-            error && ' border-red-500 dark:border-red-700'
-          )}
-          ref={ref}
-          {...props}
-        />
-      </div>
+      <input
+        type={type}
+        className={cn(
+          'border-input bg-background ring-offset-background placeholder:text-muted-foreground flex h-10 w-full rounded-md border px-3 py-1 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+          error
+            ? 'border-red-500 text-red-900 placeholder:text-red-300 focus-visible:ring-red-500 dark:border-red-400 dark:text-red-100 dark:placeholder:text-red-300 dark:focus-visible:ring-red-400'
+            : 'focus-visible:ring-ring',
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
     );
   }
 );
